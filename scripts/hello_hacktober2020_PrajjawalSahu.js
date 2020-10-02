bb21efb598550948268541166b7e26fec86d90b8
Abhishek Maira <abhishekmaira1999@gmail.com>
2020-10-02 17:51:34 +0530

    Revert "Revert "hello hacktoberfest in javascript""

---
+++ scripts/hello_hacktober2020_PrajjawalSahu.js
@@ -0,0 +1,7 @@
+//  LANGUAGE: JavaScript
+//  AUTHOR: Prajjawal Sahu
+//  GITHUB: https://github.com/PrajjawalSahu
+
+
+console.log('Hello Hacktober2020');             //will appear in console
+alert("Hello Hacktober2020");                   //displays an alert box over the current window
