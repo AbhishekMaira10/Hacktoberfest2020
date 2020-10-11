666dd327475743ac534ac54af74a5084ac70882d
jackson132002 <72300636+jackson132002@users.noreply.github.com>
2020-10-11 23:35:36 +0530

    Create hello_hacktober_jackson132002.cpp
    3rd Pull Req

---
+++ hello_hacktober_jackson132002.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR:  Prerit Agrawal  
+// GITHUB: https://github.com/jackson132002
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Hello Hacktober 2020";
+  return 0;
+}
