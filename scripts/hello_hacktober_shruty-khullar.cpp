5714548fbbcecbd4ffd07ac98ca177412fad214b
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2020-10-11 21:04:27 +0530

    Delete hello_hacktober_shruty-khullar.cpp

--- scripts/hello_hacktober_shruty-khullar.cpp
+++
@@ -1,9 +0,0 @@
-// LANGUAGE: C++
-// AUTHOR: Shruty Khullar
-// GITHUB: https://github.com/shruty-khullar
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<'Hello Hacktober2020';
-}
