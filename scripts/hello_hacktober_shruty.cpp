c3193dc7969173a771ac28b197b7479c14183261
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2020-10-11 21:05:50 +0530

    Create hello_hacktober_shruty.cpp

---
+++ scripts/hello_hacktober_shruty.cpp
@@ -0,0 +1,9 @@
+// LANGUAGE: C++
+// AUTHOR: Shruty Khullar
+// GITHUB: https://github.com/shruty-khullar
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<'Hello Hacktober2020';
+}
