a491fbaa240bb2ee389fbaf90fbfea55386f3bea
Kriti <69788174+Kriti-bit@users.noreply.github.com>
2020-10-12 09:50:01 +0530

    Create Hello_Hacktober_Kriti.cpp

---
+++ scripts/Hello_Hacktober_Kriti.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR: Kriti Singhal
+// GITHUB: https://github.com/Kriti-bit
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Welcome to Hacktober 2020";
+  return 0;
+}
