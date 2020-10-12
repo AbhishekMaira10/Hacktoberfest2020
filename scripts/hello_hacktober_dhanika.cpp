e1363534946e83b7f499c5520ad121ff763b4deb
Dhanika Grover <dhanikagrover08@gmail.com>
2020-10-12 13:40:09 +0530

    pr2

---
+++ scripts/hello_hacktober_dhanika.cpp
@@ -0,0 +1,11 @@
+// LANGUAGE: C++
+// AUTHOR:  Dhanika Grover
+// GITHUB: https://github.com/dhanika08
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Hacktober 2020";
+    return 0;
+}
