d097e2c729fc4edd20e14519e6b6321e831bc863
MisbahudDin <72780676+Misbahud-Din@users.noreply.github.com>
2020-10-13 22:23:07 +0500

    Created Hello_Hacktober2020_MisbahudDin.cpp :)

---
+++ scripts/Hello_Hacktober2020_MisbahudDin.cpp
@@ -0,0 +1,15 @@
+/*
+Language: C++
+Author: Misbahud Din
+Github: https://github.com/Misbahud-Din
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Hacktober2020" << endl;
+    return 0;
+}
