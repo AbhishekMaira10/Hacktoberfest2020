43d9c877e43df424403c5869aa08fc7aed9d06ec
sandi-aris <sandi.arisfficial@gmail.com>
2020-10-14 10:55:07 +0700

    Create 1 file java and 1 file .md

---
+++ scripts/hello_hacktober_Sandiaris.java
@@ -0,0 +1,12 @@
+package com.me.vd.main;
+
+//LANGUAGE: Java
+//AUTHOR: Sandi Aris
+//GITHUB: https://github.com/sandi-aris
+
+public class hello_hacktober_Sandiaris {
+	public static void main(String args[]) {
+	System.out.println("Hello Hacktober2020");
+
+	}
+}
