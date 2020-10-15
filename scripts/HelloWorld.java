ec4a3db4c03a29ebee89762670a0433a3c2d9ce5
OarabileMwiya <42150074+OarabileMwiya@users.noreply.github.com>
2020-10-15 04:10:48 +0200

    Add files via upload

---
+++ scripts/HelloWorld.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * 
+ * //LANGUAGE: Java
+ * //AUTHOR: Oarabile Mwiya
+ * //GITHUB: https://github.com/Oarabile Mwiya
+ */
+public class HelloWorld
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class HelloWorld
+     */
+    public HelloWorld()
+    {
+        // initialise instance variables
+       
+    }
+
+   public static void main (String [] args) {
+    //Output Hello Hacktober2020
+    System.out.println("Hello Hacktober2020");
+    }
+}
