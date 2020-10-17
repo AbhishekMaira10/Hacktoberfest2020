95c369e15a391078a683fd3e154d4153494cdfa7
Akrith H Nayak <akrithnayak2000@gmail.com>
2020-10-17 13:42:48 +0530

    Create HelloHacktobertwentytwenty.java

---
+++ scripts/HelloHacktobertwentytwenty.java
@@ -0,0 +1,21 @@
+public class HelloHacktobertwentytwenty
+{
+    // instance variables - replace the example below with your own
+    
+
+    /**
+     * Constructor for objects of class HelloWorld
+     */
+    public HelloHacktobertwentytwenty()
+    {
+        // initialise instance variables
+        System.out.println("Start Hacking!!");
+       
+    }
+
+   public static void main (String [] args) {
+    //Output Hello Hacktober2020
+    System.out.println("Hello Hacktober2020");
+    HelloHacktobertwentytwenty ob = new HelloHacktobertwentytwenty();
+    }
+}
