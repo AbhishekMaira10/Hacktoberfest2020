9b21d0b570b69e79c27ded866576f671186a0156
Lina Castro <linacastrodev@gmail.com>
2020-10-19 19:34:29 -0500

    changeson profiles and scripts

---
+++ scripts/Hello-hacktoberfest-linacastro.js
@@ -0,0 +1,5 @@
+// LANGUAGE: Javascript
+// AUTHOR: Lina Castro
+// GITHUB: https://github.com/lirrumscode
+
+console.log("Hello HacktoberFest2020 from Colombia on Community Ubuntu Colombia, from Lirrumscode 🚀")
