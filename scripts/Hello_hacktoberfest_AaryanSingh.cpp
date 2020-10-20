fa316c82613ab4668538b2b03cdff5548dd1a933
Aaryan376 <61610837+Aaryan376@users.noreply.github.com>
2020-10-20 11:45:46 +0530

    Added code

---
+++ scripts/Hello_hacktoberfest_AaryanSingh.cpp
@@ -0,0 +1,13 @@
+// Name - Aaryan Singh
+// Github profile - https://github.com/Aaryan376
+// language - Cpp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello Hacktoberfest2020";
+
+    return 0;
+}
