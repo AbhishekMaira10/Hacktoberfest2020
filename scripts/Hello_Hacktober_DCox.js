a119e8823363033016064093a8855e0e436b8ebd
DCox2016 <leadsingerstarke@yahoo.com>
2020-10-31 18:33:01 -0400

    Add console log Hello Hacktober2020 using Javascript

---
+++ scripts/Hello_Hacktober_DCox.js
@@ -0,0 +1,4 @@
+// LANGUAGE: Javascript
+// AUTHOR: Dustin Cox
+// GITHUB: https://github.com/DCox2016
+console.log("%cHello Hacktober2020", 'font-weight: bold; font-family:Snell Roundhand, cursive; font-size: 25px;color: white ; text-shadow: 3px 3px 0 #183d5d, 6px 6px 0 #ff8ae2', "🎃");
